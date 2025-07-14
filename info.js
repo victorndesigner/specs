77c629b6211c367f90f1a5e19bd95b67702416cf
bolttexturas <137934167+victorndesigner@users.noreply.github.com>
2025-07-13 23:39:47 -0300

    att

---
+++ info.js
@@ -0,0 +1,46 @@
+const servidorNome = "E_Dream-X0017";
+
+const blocosInfo = [
+  {
+    nome: "bolttexturas",
+    id: "150798485",
+    mostrar: true,
+    imagem: "assets/images/bolttexturas.png"
+  },
+  {
+    nome: "boltalt01",
+    id: "150821550",
+    mostrar: true,
+    imagem: "assets/images/boltalt01.png"
+  },
+  {
+    nome: "boltalt02",
+    id: "150875749",
+    mostrar: true,
+    imagem: "assets/images/boltalt02.png"
+  },
+  {
+    nome: "boltalt03",
+    id: "150824632",
+    mostrar: true,
+    imagem: "assets/images/boltalt03.png"
+  },
+  {
+    nome: "bolt-alt04",
+    id: "150824634",
+    mostrar: true,
+    imagem: "assets/images/bolt-alt04.png"
+  },
+  {
+    nome: "boltalt05",
+    id: "150824641",
+    mostrar: true,
+    imagem: "assets/images/boltalt05.png"
+  },
+  {
+    nome: "boltalt06",
+    id: "150899727",
+    mostrar: true,
+    imagem: "assets/images/boltalt06.png"
+  }
+];
