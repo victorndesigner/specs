d9b455357830488bdefdaed5998ceb72874b66bb
bolttexturas <137934167+victorndesigner@users.noreply.github.com>
2025-08-03 17:54:35 -0300

    att

--- info.js
+++ info.js
@@ -10,37 +10,49 @@ const blocosInfo = [
   {
     nome: "boltalt01",
     id: "150821550",
-    mostrar: false,
+    mostrar: true,
     imagem: "assets/images/boltalt01.png"
   },
   {
     nome: "boltalt02",
     id: "150875749",
-    mostrar: false,
+    mostrar: true,
     imagem: "assets/images/boltalt02.png"
   },
   {
     nome: "boltalt03",
     id: "150824632",
-    mostrar: false,
+    mostrar: true,
     imagem: "assets/images/boltalt03.png"
   },
   {
     nome: "bolt-alt04",
     id: "150824634",
-    mostrar: false,
+    mostrar: true,
     imagem: "assets/images/bolt-alt04.png"
   },
   {
     nome: "boltalt05",
     id: "150824641",
-    mostrar: false,
+    mostrar: true,
     imagem: "assets/images/boltalt05.png"
   },
   {
     nome: "boltalt06",
     id: "150899727",
-    mostrar: false,
+    mostrar: true,
     imagem: "assets/images/boltalt06.png"
+  },
+  {
+    nome: "boltalt07",
+    id: "150899734",
+    mostrar: true,
+    imagem: "assets/images/boltalt07.png"
+  },
+  {
+    nome: "boltalt08",
+    id: "150901252",
+    mostrar: true,
+    imagem: "assets/images/boltalt08.png"
   }
 ];
